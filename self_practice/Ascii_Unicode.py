945f5499c5d98adef34c9195d0c0f8e33b74eaeb
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-05 15:55:48 +0530

    explored ASCII & Unicode values

---
+++ self_practice/Ascii_Unicode.py
@@ -0,0 +1,22 @@
+#Ascii and Unicode values of a character
+# Ascii values of a character
+print(ord('a'))
+print(ord('A'))
+# Unicode values of a character
+print(ord('€'))
+print(ord('¥'))
+# Character of an Ascii value
+print(chr(97))
+print(chr(65))
+# Character of a Unicode value
+print(chr(8364))
+print(chr(165))
+# Output:
+# 97
+# 65
+# 8364
+# 165
+# a
+# A
+# €
+# ¥
