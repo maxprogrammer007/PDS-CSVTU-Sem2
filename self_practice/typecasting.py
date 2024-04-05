024c9e9eb92f7b83123558a88c6974f5129d6f08
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-05 17:28:28 +0530

    Exploring Typecasting

---
+++ self_practice/typecasting.py
@@ -0,0 +1,126 @@
+#Typecasting in python
+#Typecasting is the process of converting one data type to another data type.
+#There are two types of typecasting:
+#Implicit Typecasting
+#Explicit Typecasting
+#Implicit Typecasting
+#Implicit typecasting is done automatically by the compiler.
+#In this type of typecasting, the compiler automatically converts one data type to another data type.
+#Implicit typecasting is also known as automatic typecasting.
+#Implicit typecasting is done when:
+#A smaller data type is converted to a larger data type.
+#For example, converting an integer to a float.
+#Explicit Typecasting
+#Explicit typecasting is done manually by the programmer.
+#In this type of typecasting, the programmer manually converts one data type to another data type.
+#Explicit typecasting is also known as manual typecasting.
+#Explicit typecasting is done when:
+#A larger data type is converted to a smaller data type.
+#For example, converting a float to an integer.
+#Typecasting functions in python
+#There are several typecasting functions in python that are used to convert one data type to another data type.
+#The following are the typecasting functions in python:
+#int(): This function is used to convert a data type to an integer data type.
+#float(): This function is used to convert a data type to a float data type.
+#str(): This function is used to convert a data type to a string data type.
+#bool(): This function is used to convert a data type to a boolean data type.
+#list(): This function is used to convert a data type to a list data type.
+#tuple(): This function is used to convert a data type to a tuple data type.
+#set(): This function is used to convert a data type to a set data type.
+#dict(): This function is used to convert a data type to a dictionary data type.
+#complex(): This function is used to convert a data type to a complex data type.
+#Typecasting examples
+#The following are the examples of typecasting in python:
+#Converting an integer to a float
+x = 10
+y = float(x)
+print(y)
+#Converting a float to an integer
+x = 10.5
+y = int(x)
+print(y)
+#Converting a string to an integer
+x = "10"
+y = int(x)
+print(y)
+#Converting an integer to a string
+x = 10
+y = str(x)
+print(y)
+#Converting a string to a float
+x = "10.5"
+y = float(x)
+print(y)
+#Converting a float to a string
+x = 10.5
+y = str(x)
+print(y)
+#Converting a string to a boolean
+x = "True"
+y = bool(x)
+print(y)
+#Converting a boolean to a string
+x = True
+y = str(x)
+print(y)
+#Converting a list to a tuple
+x = [10, 20, 30, 40, 50]
+y = tuple(x)
+print(y)
+#Converting a tuple to a list
+x = (10, 20, 30, 40, 50)
+y = list(x)
+print(y)
+#Converting a string to a set
+x = "hello"
+y = set(x)
+print(y)
+#Converting a set to a string
+x = {"h", "e", "l", "l", "o"}
+y = str(x)
+print(y)
+#Converting a dictionary to a string
+x = {'name': 'John', 'age': 25}
+y = str(x)
+print(y)
+#Converting a string to a complex number
+x = "10+5j"
+y = complex(x)
+print(y)
+#Converting a complex number to a string
+x = 10+5j
+y = str(x)
+print(y)
+#Converting a string to a list
+x = "hello"
+y = list(x)
+print(y)
+#Converting a list to a string
+x = ['h', 'e', 'l', 'l', 'o']
+y = str(x)
+print(y)
+#Converting a string to a tuple
+x = "hello"
+y = tuple(x)
+print(y)
+#Converting a tuple to a string
+x = ('h', 'e', 'l', 'l', 'o')
+y = str(x)
+print(y)
+#Converting a string to a set
+x = "hello"
+y = set(x)
+print(y)
+#Converting a set to a string
+x = {'h', 'e', 'l', 'l', 'o'}
+y = str(x)
+print(y)
+#Converting a dictionary to a string
+x = {'name': 'John', 'age': 25}
+y = str(x)
+print(y)
+#Converting a string to a complex number
+x = "10+5j"
+y = complex(x)
+print(y)
+
