2623f90a0ad52b25463ff3d4078246269eeabd11
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-05 15:45:51 +0530

    used sep and end

---
+++ self_practice/end_sep.py
@@ -0,0 +1,7 @@
+#printing with separator
+print("Hello", "World", sep="***")
+# printing with end
+print("Hello", end=" ")
+print("World")
+# printing with end and separator
+print("Hello", "World", sep="***", end="!!!")
