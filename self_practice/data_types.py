0af82f21a099c60a8d322764ee7782886cdaf841
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-05 15:49:57 +0530

    explored data types

---
+++ self_practice/data_types.py
@@ -0,0 +1,30 @@
+#Numeric Data Types
+#int
+int = 100
+print("This is ",type(int))
+#float
+float = 100.0
+print("This is ",type(float))
+#complex
+complex = 1+2j
+print("This is ",type(complex))
+#Sequence Data Types
+#String
+string = "Hello"
+print("This is ",type(string))
+#List
+list = [1,2,3,4,5]
+print("This is ",type(list))
+#Tuple
+tuple = (1,2,3,4,5)
+print("This is ",type(tuple))
+#Set
+set = {1,2,3,4,5}
+print("This is ",type(set))
+#Dictionary
+dict = {"name":"John", "age":25}
+print("This is ",type(dict))
+#Boolean Data Type
+boolean = True
+print("This is ",type(boolean))
+
