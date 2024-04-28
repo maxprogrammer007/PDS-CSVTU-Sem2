6d397b33f3edea843ac464b3f10b418632a9f278
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-28 13:46:33 +0530

    F_string implementations

---
+++ Concept_Implementations/f_strings.py
@@ -0,0 +1,4 @@
+# Ask user for name and age and print a message with that information
+name = input("What is your name? ")
+age = input("What is your age? ")
+print(f"Hello {name}! You are {age} years old.")
