33b4f4c0e03b2ef8500b69f7cbc1c6aff31443e2
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-28 13:50:17 +0530

    Create removing_whitespaces.py

---
+++ Concept_Implementations/removing_whitespaces.py
@@ -0,0 +1,12 @@
+# Ask user for the input
+
+name = input("what is your name ? ")
+
+# remove whitespace from the str
+
+name = name.strip()
+
+# strip() removes whitespaces from the beginning and end of the string
+
+print(f"Hello {name}!")
+# The output will be "Hello name!" without any whitespaces in the beginning or end of the string
