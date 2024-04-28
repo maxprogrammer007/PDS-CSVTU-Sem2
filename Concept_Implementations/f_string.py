8b3352692b11ee48fb82185e494b8c78a474807b
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-28 13:44:21 +0530

    using the formatted string implementations

---
+++ Concept_Implementations/f_string.py
@@ -0,0 +1,10 @@
+# Ask user for there name and age and print it using f-string
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello {name}, you are {age} years old.")
+
+# Output:
+# Enter your name: John
+# Enter your age: 25
+# Hello John, you are 25 years old.
