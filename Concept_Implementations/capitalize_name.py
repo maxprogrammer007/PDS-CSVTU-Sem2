16940c0cf64c14fcc1ce0e0ea431f7e895abab25
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-29 01:09:43 +0530

    ok

---
+++ Concept_Implementations/capitalize_name.py
@@ -0,0 +1,11 @@
+# ASk user input
+name = input("What is your name deer ?")
+
+# Capitalize the name
+
+name = name.capitalize()
+
+# capitalize() capitalizes the first letter of the string
+
+
+print(f"Hello {name}!")
